2194fb5273931661c75eb554c039a67533913cd2
Guilherme <guilherme.cavalcanti87@gmail.com>
2023-03-15 10:34:48 -0300

    exercícios

---
+++ Aula_01_exercícios/variaveis.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Olá Gui futuro ADS"); //você pode dar print com " ou ' (mantenha um padrão_)
+  
+  String nome = "Guilherme Martins Cavalcanti"; //representa uma variável textual
+  int idade = 35;
+  double altura = 1.85;
+  bool fumante = false;
+  
+  print("Nome: $nome");
+  print("Idade: $idade");
+  print("Altura: $altura");
+  print("Fuma? $fumante");
+}
